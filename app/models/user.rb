95ebb713231036bb54e818f3ec5bcf0b12eb1838
KoichiYamamoto <ko1y5osaka@gmail.com>
2020-11-04 03:00:01 +0000

    [Start4]

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+  has_many :followings, through: :active_relationships, source: :follower
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followers, through: :passive_relationships, source: :following
   attachment :profile_image, destroy: false
   
   def 
@@ -14,6 +18,9 @@ class User < ApplicationRecord
 	  self.favorites.exists?(book_id: book.id)
   end
   
+  def followed_by?(user)
+    passive_relationships.find_by(following_id: user.id).present?
+  end
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: { maximum: 50 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  get 'relationships/create'
+  get 'relationships/destroy'
   root 'homes#top'
   get 'home/about' => 'homes#about', as: 'about'
   devise_for :users
-  resources :users, only: [:show,:index,:edit,:update]
+  resources :users, only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get :follows, on: :member 
+    get :followers, on: :member
+  end
   resources :books do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
