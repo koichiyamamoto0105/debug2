b896bcdec2b0ca216d02025375cc9b62eb3329c6
KoichiYamamoto <ko1y5osaka@gmail.com>
2020-11-05 09:48:35 +0000

    [submit5]

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about', as: 'about'
   devise_for :users
   resources :users, only: [:show,:index,:edit,:update] do
+    # collection do
+    #   get 'search'
+    # end
     resource :relationships, only: [:create, :destroy]
     get :follows, on: :member 
     get :followers, on: :member
@@ -13,4 +16,7 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end  
+  
+  get 'search' => 'searches#search'
+  
 end
